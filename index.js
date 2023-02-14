02ff976d89635150c697ebaa377d684d6b243e43
CesarQuint <pedrocesarquintero001@gmail.com>
2023-02-13 22:41:02 -0600

    Cors Fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv'
+import cors from 'cors'
 import conectarDB from "./config/db.js";
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import proyectoRoutes from './routes/proyectoRoutes.js'
@@ -11,6 +12,23 @@ app.use(express.json())
 dotenv.config()
 
 conectarDB()
+
+// Configurar CORS
+const whitelist =[process.env.FRONTEND_URL,process.env.FRONTEND_URL2]
+
+const corsOption={
+    origin:function(origin,callback){
+        if(whitelist.includes(origin)){
+            //Puede Consultar API
+            callback(null,true)
+        }else{
+            //No esta permitido el Request
+            callback(new Error("Error de Cors"))
+        }
+    }
+}
+
+app.use(cors(corsOption))
 
 //Routing
 app.use("/api/usuarios",usuarioRoutes)
